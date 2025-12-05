4c86c2474fdd31675ea025563bf7771731fc8beb
lucaslasanta <123886204+lucaslasanta@users.noreply.github.com>
2025-12-05 16:56:46 +0000

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: 'sans-serif' }}>
+      <h1>Le Saint Scan App</h1>
+      <p>This is the backend system for bottle QR scans.</p>
+      <p>Visit /bottle/0001 to test.</p>
+    </div>
+  );
+}
