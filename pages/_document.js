7184260adb13ce7a1cae92a7bbb651c8f975eb7f
lucaslasanta <123886204+lucaslasanta@users.noreply.github.com>
2025-12-07 19:48:51 +0000

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,14 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@300;400&family=Inter:wght@300;400;500&display=swap"
           rel="stylesheet"
         />
+
+        {/* FIX iOS MOBILE ZOOM + CONTENT SHIFTING */}
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
       </Head>
+
       <body style={{ margin: 0, backgroundColor: "black" }}>
         <Main />
         <NextScript />
